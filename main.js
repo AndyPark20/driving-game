e6d10a86f494447251332d43df5809384142db61
Andy Park <apark1214@gmail.com>
2020-12-11 13:30:08 -0800

    added keydown initation that moves the car's direction

--- main.js
+++ main.js
@@ -5,15 +5,20 @@ var myCar ={
 };
 
 document.addEventListener('keydown',function(e){
-
+  $varImage.className ='';
   if(e.keyCode ===39){
     myCar.directions='east';
+    $varImage.classList.add(myCar.directions);
   }else if(e.keyCode === 40){
     myCar.directions ='south';
+    $varImage.classList.add(myCar.directions);
   }else if (e.keyCode ===37){
     myCar.directions ='west';
+    $varImage.classList.add(myCar.directions);
   }else if (e.keyCode ===38){
     myCar.directions= 'north';
+    $varImage.classList.add(myCar.directions)
   }
+
 
 })
