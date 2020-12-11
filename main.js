63b2f026eb55278575fd84e5bf9fc2fc346d252f
Andy Park <apark1214@gmail.com>
2020-12-11 15:17:06 -0800

    verified that the car moves in the hit of a spaceBar and x,y coordinates are recorded in the myCar object

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
-$varImage = document.querySelector('img');
+var $varImage = document.querySelector('img');
+var $carContainer = document.querySelector('.carStyle');
+
 
 var myCar ={
-  x-coordinate:'',
-  y-coordinate:'',
+  xCoordinate:'',
+  yCoordinate:'',
   directions:''
 };
 
 var intervalId=null;
+var distanceX=0;
+var distanceY=0;
 
 document.addEventListener('keydown',function(e){
+  clearInterval(intervalId);
   $varImage.className ='';
   if(e.keyCode ===39){
     myCar.directions='east';
@@ -22,6 +27,14 @@ document.addEventListener('keydown',function(e){
   }else if (e.keyCode ===38){
     myCar.directions= 'north';
     $varImage.classList.add(myCar.directions)
+  }if (e.keyCode ===32){
+    intervalId=setInterval(function(){
+      distanceX+=15;
+      myCar.xCoordinate=distanceX;
+      myCar.yCoordinate=distanceY;
+      $carContainer.style.left = myCar.xCoordinate + 'px';
+      $carContainer.style.top = myCar.yCoordinate + 'px';
+    },16)
   }
 
 
